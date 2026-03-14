02827927d50b621afc684c1e65954c8a07f44d6e
Hazyyyq <haziqirsyad1609@gmail.com>
2026-03-14 22:41:47 +0800

    Add Balance and Bank Card Components, Update Home Page, and Modify Dependencies

--- wallet_apps/lib/Pages/home_page.dart
+++ wallet_apps/lib/Pages/home_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:wallet_apps/Components/balance_card.dart';
+import 'package:wallet_apps/Components/bank_card.dart';
 
 // ================= Home Screen ===============
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key, 
-    required this.title,
-    required this.username
-    });
+  const MyHomePage({super.key, required this.title, required this.username});
 
   final String title;
   final String username;
@@ -17,6 +16,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // Controller
+  final page_controller = PageController();
+  int _currentPage = 0; // Track the current page index
+
+  @override
+  void dispose() {
+    page_controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,9 +38,12 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Row(
                 children: [
-
                   // profile picture
-                  const Icon(Icons.person_3_rounded, size: 45, color: Color.fromARGB(255, 63, 61, 61),),
+                  const Icon(
+                    Icons.person_3_rounded,
+                    size: 45,
+                    color: Color.fromARGB(255, 63, 61, 61),
+                  ),
                   SizedBox(width: 10),
 
                   // welcome message
@@ -63,76 +75,95 @@ class _MyHomePageState extends State<MyHomePage> {
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
                         BoxShadow(
-                          color: const Color.fromARGB(255, 118, 117, 117).withOpacity(0.5),
+                          color: const Color.fromARGB(
+                            255,
+                            118,
+                            117,
+                            117,
+                          ).withOpacity(0.5),
                           spreadRadius: 2,
                           blurRadius: 5,
-                          offset: Offset(0, 3), // changes position of shadow
+                          offset: Offset(0, 3), // shadow offset
                         ),
                       ],
                     ),
-                    child: Icon(Icons.add, color: const Color.fromARGB(255, 0, 0, 0),),
+                    child: Icon(
+                      Icons.add,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
                   ),
-
                 ],
               ),
             ),
 
-            SizedBox(height: 25,),
-            // cards
+            SizedBox(height: 25),
 
+            // cards
             Container(
               height: 200,
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 63, 61, 61),
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromARGB(255, 118, 117, 117).withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3), // changes position of shadow
+              child: PageView(
+                controller: page_controller,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentPage = index;
+                  });
+                },
+                scrollDirection: Axis.horizontal,
+                children: [
+                  BalanceCard(balance: "150.50", cardColour: "0xFF000000"),
+                  // Maybank
+                  BankCard(
+                    username: widget.username,
+                    balance: "2,450.50",
+                    expiryDate: "12/28",
+                    cardNumber: "**** **** **** 1234",
+                    cardType: "Maybank",
+                    cardColour: "0xFFE5B800", // Maybank Dark Yellow
+                  ),
+
+                  // RHB
+                  BankCard(
+                    username: widget.username,
+                    balance: "1,120.00",
+                    expiryDate: "05/27",
+                    cardNumber: "**** **** **** 8890",
+                    cardType: "RHB",
+                    cardColour: "0xFF5BC2E7", // RHB Blue
+                  ),
+
+                  // Bank Rakyat
+                  BankCard(
+                    username: widget.username,
+                    balance: "5,300.20",
+                    expiryDate: "09/26",
+                    cardNumber: "**** **** **** 4456",
+                    cardType: "BankRakyat",
+                    cardColour: "0xFF005CAB", // Bank Rakyat Blue
                   ),
                 ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '**** **** **** 1234',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        letterSpacing: 2,
-                      ),
-                    ),
+            ),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          widget.username.toUpperCase(),
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
+            // dot indicator
+            const SizedBox(height: 15),
 
-                        Text(
-                          '12/24',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            SmoothPageIndicator(
+              controller: page_controller,
+              count: 4,
+              effect: ExpandingDotsEffect(
+                activeDotColor: Colors.black,
+                dotColor: Colors.grey.withOpacity(0.4),
+                dotHeight: 8,
+                dotWidth: 8,
+                expansionFactor: 3,
               ),
+              onDotClicked: (index) {
+                page_controller.animateToPage(
+                  index,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeInOut,
+                );
+              },
             ),
 
             // buttons
